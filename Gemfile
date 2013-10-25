c7d18942efbfa673e9bbd1aea753ff6b5afaf106
Scott McCormack <scott.mccormack@tapjoy.com>
2013-10-24 20:53:00 -0400

    Almost done

--- Gemfile
+++ Gemfile
@@ -1,15 +1,25 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+group :development do
+  gem 'sqlite3'
+end
+
+
+group :production do
+  gem 'pg'
+end
 
 gem 'puma'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
+
+gem 'rack-jsonp'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/flag_controller.rb
+++ app/controllers/flag_controller.rb
@@ -1,19 +1,64 @@
 class FlagController < ApplicationController
 
   def index
-    render json: response
+    # render text: "callback(#{response_data.to_json})"
+    render json: response_data
   end
 
   private
 
     def direction
+      dirs = %w(N E S W)
+      dirs.reject! { |dir| %W(WALL OOB).include? tiles[dir] }
+      puts dirs.inspect
+      dirs.sort! do |a,b|
+        if care_about.include?(a)
+          if care_about.include?(b)
+            0
+          else
+            1
+          end
+        else
+          if care_about.include?(b)
+            -1
+          else
+            0
+          end
+        end
+      end
+      puts dirs.inspect
 
+      if dirs.length > 1
+        return dirs.second if Random.new.rand < 0.25
+      end
+      return dirs.first
     end
 
-    def response
+    def response_data
       {
         team: 5,
         nextCommand: direction
       }
+    end
+
+    def x
+      tiles['location']['x']
+    end
+
+    def y
+      tiles['location']['y']
+    end
+
+    def furthest
+      @furthest ||= x.abs > y.abs ? :x : :y
+    end
+
+    def care_about
+      return %w(E W) if furthest == :x
+      return %w(N S) if furthest == :y
+    end
+
+    def tiles
+      @tiles ||= JSON.parse params[:tiles]
     end
 end
